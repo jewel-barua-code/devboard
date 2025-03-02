75b05eb265c59e01b674b0c75b30a227757092c0
jewelCode <jewelbarua101@gmail.com>
2025-03-02 19:50:56 +0600

    random color added

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 document.getElementById('discover').addEventListener('click', function(event){
     window.location.href = './blogs.html'
 })
+
+document.getElementById('background-color').addEventListener('click', function(){
+    var x = Math.floor(Math.random() * 256);
+    var y = Math.floor(Math.random() * 256);
+    var z = Math.floor(Math.random() * 256);
+    var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+    document.getElementById('body-color').style.backgroundColor = bgColor;
+})
